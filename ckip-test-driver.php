3194d54803415c3c280066f9336b8f4b601c2ed7
lijyue925 <j245700@gmail.com>
2015-04-19 22:35:53 +0800

    增加 Namespace 調整 Class

--- ckip-test-driver.php
+++ ckip-test-driver.php
@@ -1,18 +1,6 @@
 <?php
-/**
- * ckip-test-driver.php
- *
- * PHP version 5
- *
- * @category PHP
- * @package  /
- * @author   Fukuball Lin <fukuball@gmail.com>
- * @license  No Licence
- * @version  Release: <1.0>
- * @link     http://fukuball@github.com
- */
-
-require_once "src/CKIPClient.php";
+use Lijyue925\CKIP\CKIPClient;
+require_once('src/CKIPClient.php');
 
 // change to yours
 define("CKIP_SERVER", "000.000.000.000");
@@ -20,23 +8,15 @@ define("CKIP_PORT", 0000);
 define("CKIP_USERNAME", "xxxxxx");
 define("CKIP_PASSWORD", "xxxxxxxxx");
 
-$ckip_client_obj = new CKIPClient(
-   CKIP_SERVER,
-   CKIP_PORT,
-   CKIP_USERNAME,
-   CKIP_PASSWORD
+$CKIP = new CKIPClient(
+    CKIP_SERVER,
+    CKIP_PORT,
+    CKIP_USERNAME,
+    CKIP_PASSWORD
 );
 
-$raw_text = "獨立音樂需要大家一起來推廣，\n".
-            "歡迎加入我們的行列！。\n";
+$raw_text = "站在巨人的肩膀上，一步一步充實自己。";
 
-$return_text = $ckip_client_obj->send($raw_text);
-echo $return_text;
+print_r($CKIP->getSentence($raw_text));
 
-$return_sentence = $ckip_client_obj->getSentence();
-print_r($return_sentence);
-
-$return_term = $ckip_client_obj->getTerm();
-print_r($return_term);
-
-?>
+print_r($CKIP->getTerm($raw_text));
--- src/CKIPClient.php
+++ src/CKIPClient.php
@@ -1,33 +1,21 @@
-<?php
+<?php namespace Lijyue925\CKIP;
 
-/**
- * CKIPClient.php
- *
- * PHP version 5
- *
- * @category PHP
- * @package  /
- * @author   Fukuball Lin <fukuball@gmail.com>
- * @license  No Licence
- * @version  Release: <1.0>
- * @link     http://fukuball@github.com
- */
+use xmlWriter;
+
 class CKIPClient {
-    private $test;
+
     private $server_ip;
     private $server_port;
     private $username;
     private $password;
 
-    public $raw_text;
-    public $return_text;
-    public $sentence = array();
-    public $term = array();
-
     /**
      * Method __construct initialize instance
      *
-     * @return void
+     * @param $server_ip
+     * @param $server_port
+     * @param $username
+     * @param $password
      */
     public function __construct ($server_ip, $server_port, $username, $password)
     {
@@ -37,26 +25,22 @@ class CKIPClient {
         $this->username = $username;
         $this->password = $password;
 
-    }// end function __construct
+    }
 
     /**
      * Method send
      *
      * @param string $raw_text
      *
-     * @return string $return_text
+     * @return string $result
      */
-    public function send ($raw_text)
+    private function send ($raw_text)
     {
-
         if (! empty($raw_text))
         {
-
-            $this->raw_text = $raw_text;
-
+            /*$this->raw_text = $raw_text;*/
             $xw = new xmlWriter();
             $xw->openMemory();
-
             $xw->startDocument('1.0');
 
             $xw->startElement('wordsegmentation');
@@ -71,7 +55,7 @@ class CKIPClient {
             $xw->endElement();
 
             $xw->startElement('text');
-            $xw->writeRaw($this->raw_text);
+            $xw->writeRaw($raw_text);
             $xw->endElement();
 
             $xw->endElement();
@@ -85,88 +69,67 @@ class CKIPClient {
             $socket = socket_create(AF_INET, SOCK_STREAM, $protocol);
             socket_connect($socket, $this->server_ip, $this->server_port);
             socket_write($socket, $message);
-            $this->return_text = iconv("big5", "utf-8", socket_read($socket, strlen($message) * 3));
-
+            $result = iconv("big5", "utf-8", socket_read($socket, strlen($message) * 3));
             socket_shutdown($socket);
             socket_close($socket);
 
+            return $result;
         }
 
-        return $this->return_text;
-
-    }// end function send
+        return null;
+    }
 
     /**
      * Method getSentence
      *
-     * @return array $return_sentence
+     * @return array $sentences
      */
-    public function getSentence ()
+    public function getSentence ($raw_text)
     {
-
-        // empty the array
-        $this->sentence = array();
-
-        if ($parse_return_text = simplexml_load_string($this->return_text))
+        $return_text = $this->send($raw_text);
+        $sentences = [];
+        if ($parse_return_text = simplexml_load_string($return_text))
         {
-
             $sentence_array = $parse_return_text->result->sentence;
-
             foreach ($sentence_array as $key => $sentence)
             {
-
                 $sentence_value = (string) $sentence;
                 // remove invisible characters
                 $check_sentence = preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $sentence_value);
-
                 if (! empty($check_sentence))
                 {
-                    array_push($this->sentence, $sentence_value);
+                    array_push($sentences, $sentence_value);
                 }
-
             }
-
         }
 
-        return $this->sentence;
-
-    }// end function getSentence
+        return $sentences;
+    }
 
     /**
      * Method getTerm
      *
-     * @return array $return_term
+     * @return array $terms
      */
-    public function getTerm ()
+    public function getTerm ($raw_text)
     {
-
-        // empty the array
-        $this->term = array();
-
-        $this->getSentence();
-
-        foreach ($this->sentence as $t)
+        $sentences = $this->getSentence($raw_text);
+        $terms = [];
+        foreach ($sentences as $sentence)
         {
-
-            foreach (explode("　", $t) as $s)
+            foreach (explode("　", $sentence) as $word)
             {
-
-                if ($s != "")
+                if ($word != "")
                 {
-                    preg_match("/(\S*)\((\S*)\)/", $s, $m);
-
-                    $this_term_array = array("term" => $m[1], "tag" => $m[2]);
-                    array_push($this->term, $this_term_array);
-
+                    preg_match("/(\S*)\((\S*)\)/", $word, $m);
+                    $temp_array = array("term" => $m[1], "tag" => $m[2]);
+                    array_push($terms, $temp_array);
                 }
-
             }
-
         }
 
-        return $this->term;
-
-    }// end function getTerm
+        return $terms;
+    }
 
     /**
      * Method __destruct unset instance value
@@ -175,18 +138,12 @@ class CKIPClient {
      */
     public function __destruct ()
     {
-
         $class_property_array = get_object_vars($this);
-
         foreach ($class_property_array as $property_key => $property_value)
         {
-
             unset($this->$property_key);
-
-        }// end foreach
-
-    }// end function __destruct
-
+        }
+    }
 }
 
 ?>
